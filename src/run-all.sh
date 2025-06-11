8ab1bd0a93b8f6a6d9fa83cee0ccf233ac9a5831
Torben <59419684+entorb@users.noreply.github.com>
2025-06-11 08:50:00 +0200

    minor

--- src/run-all.sh
+++ src/run-all.sh
@@ -4,12 +4,15 @@
 script_dir=$(dirname $0)
 cd $script_dir/..
 
+mkdir -p data
+mkdir -p output
+
 echo "## git_1_export_history"
 src/git_1_export_history.sh
 
 cp ../rememberthemilk/output/tasks_completed.csv data/rtm_tasks_completed.csv
 
-for job in cal git_2_analyze oura strava rtm join; do
+for job in cal git_2_analyze oura strava rtm influx_1_download influx_2 lifelog join; do
     echo "## $job"
     python3 src/$job.py
 done
