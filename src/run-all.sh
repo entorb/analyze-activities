63b8d26691d1382752727fb4c1baeccc2fe0b548
Torben <59419684+entorb@users.noreply.github.com>
2026-08-19 13:20:33 +0200

    prek: add shfmt

--- src/run-all.sh
+++ src/run-all.sh
@@ -12,35 +12,35 @@ mkdir -p output
 # only if older than 1h, start the pre-script
 f=data/git/analyze-activities.log
 if [ ! -f $f ] || [ $(find $f -mmin +60) ]; then
-    echo "## git_1_export_history"
-    src/git_1_export_history.sh
+  echo "## git_1_export_history"
+  src/git_1_export_history.sh
 fi
 
 f=data/influx-media.csv
 if [ ! -f $f ] || [ $(find $f -mmin +60) ]; then
-    echo "## influx download"
-    python src/influx_1_download.py
+  echo "## influx download"
+  python src/influx_1_download.py
 fi
 
 f=data/oura_sleep.csv
 if [ ! -f $f ] || [ $(find $f -mmin +60) ]; then
-    echo "## oura download"
-    cd ../analyze-oura
-    python src/fetch.py
-    cp data/data_sleep.csv $cwd/$f
-    cd $cwd
+  echo "## oura download"
+  cd ../analyze-oura
+  python src/fetch.py
+  cp data/data_sleep.csv $cwd/$f
+  cd $cwd
 fi
 
 f=data/rtm_tasks_completed.csv
 if [ ! -f $f ] || [ $(find $f -mmin +60) ]; then
-    echo "## rtm download"
-    cd ../rememberthemilk
-    python src/tasks_completed.py
-    cp output/tasks_completed.csv $cwd/$f
-    cd $cwd
+  echo "## rtm download"
+  cd ../rememberthemilk
+  python src/tasks_completed.py
+  cp output/tasks_completed.csv $cwd/$f
+  cd $cwd
 fi
 
 for job in cal git_2 oura strava rtm influx_2 lifelog join; do
-    echo "## $job"
-    python3 src/$job.py
+  echo "## $job"
+  python3 src/$job.py
 done
